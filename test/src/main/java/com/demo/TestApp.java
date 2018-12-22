111b25f372b6a272a55f98a3b910f0de853fafae
vinayak <vinayak@vinayak-Inspiron-3542>
2018-12-22 14:53:48 +0530

    project shared

---
+++ test/src/main/java/com/demo/TestApp.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.demo;
+
+/**
+ * @author vinayak
+ *
+ */
+public class TestApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test is running");
+		
+	}
+
+}
